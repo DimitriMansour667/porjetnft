06e20848f28edf8ce7aa0e6ef3d27b1338d0f657
Dimitri Mansour <dimi.mansour03@gmail.com>
2022-11-21 19:55:05 -0500

    1.0

--- code.py
+++ code.py
@@ -1,13 +1,22 @@
-def addnew():
+def addNew():
     file = open("data.txt", "r+")
     nftname = input("NFT NAME : ")
     nftimg = input("NFT IMG : ")
     nftOwner = input("NFT OWNER (x if no owner) : ")
     file.write(nftname + " " + nftimg + " " + nftOwner + "\n")
+    file.close
 
-action = 0
+def moveNft():
+    file = open("data.txt", "r+")
+    file.close
 
-while(action != 3) : 
+action = None
+while(action != "3") : 
     action = input("\t NFT PROJECT\n1- ADD NEW NFT\n2- MOVE NFT\n3- CLOSE\n")
+    if(action == "1") :
+        addNew()
+    elif(action == "2") :
+        moveNft()
+    
 
     
